111d87714bf298d2b3d1b77e876e1e2c2c829edf
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-01-12 11:51:07 +0530

    Added a noiseMap generator

---
+++ Landmass Generation/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,31 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
